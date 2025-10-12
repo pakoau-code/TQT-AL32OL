a868e4b1a3b8647b729ab6506bf52555c596b57a
pakoau-code <pakoau@gmail.com>
2025-10-12 00:38:30 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'tqt-al32ol-v5';
+const ASSETS = [
+  './index.html','./index-desktop.html',
+  './manifest.webmanifest','./icon-192.png','./icon-512.png','./logo-tqt.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); })))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
